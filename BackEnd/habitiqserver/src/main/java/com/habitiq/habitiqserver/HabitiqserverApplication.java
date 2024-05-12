8cc4984d50dc50e5c20cfabcc0c4fedaf20e25d2
Murali <vmkrishnan2742@gmail.com>
2024-05-12 22:22:50 +0530

    Spring init

---
+++ BackEnd/habitiqserver/src/main/java/com/habitiq/habitiqserver/HabitiqserverApplication.java
@@ -0,0 +1,13 @@
+package com.habitiq.habitiqserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitiqserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitiqserverApplication.class, args);
+	}
+
+}
---
+++ BackEnd/habitiqserver/src/test/java/com/habitiq/habitiqserver/HabitiqserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitiq.habitiqserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitiqserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
